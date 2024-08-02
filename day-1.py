b0d155335e9ff89376752b7cc542676f311eee01
BernardSMendoza <159678443+BernardSMendoza@users.noreply.github.com>
2024-08-02 01:26:12 +0100

    day 1
    decided to go with tech with tim as my friend @ClassicConar recommended. seems to be simple as I have only done tutorial 1 of 20.

---
+++ day-1.py
@@ -0,0 +1,16 @@
+# datatypes, 4 for now: int, str, bool, float.
+# int is for stuff like 1, 69, 420
+# str is for stuff like 'Bernard', 's*x', "Weed"(this is not a mistake)
+# bool is true or false
+# float is for stuff like 420.69 (decimal numbers)
+
+# ________________________
+# variable.
+age = 20
+print(age)
+
+name = 'bernard'
+print(name)
+# Don't use "" for str. For some reason they just don't work, even though they are a string
+
+
